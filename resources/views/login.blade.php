287ecd6e21127dcd648bf1283afb165a61af0f20
edwinabdiwijaya <edwin.abdiwijaya@binus.ac.id>
2023-01-05 18:45:51 +0700

    Update some changes in login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,17 +3,17 @@
 @section('title_web', 'login')
 
 @section('body')
-    <form class="d-flex flex-column align-items-center" action="/authenticate" method="post">
+    <form action="/authenticate" method="post">
         @csrf
-        <div class="col-md-3 align-items-center container-fluid justify-content-center" style="background-color: cyan">
+        <div class="d-flex flex-column align-items-center container-fluid justify-content-center" style="background-color: bisque">
             <h1 class="text-center">Login</h1>
-            <div class="mb-3">
+            <div class="col-md-6 mb-4">
                 <label for="email" class="form-label">Email</label>
-                <input type="email" class="form-control" name="email" id="email">
+                <input type="email" class="form-control" id="email">
             </div>
-            <div class="mb-3">
+            <div class="col-md-6 mb-4">
                 <label for="password" class="form-label">Password</label>
-                <input type="password" class="form-control" name="password" id="password" placeholder="">
+                <input type="password" class="form-control" id="password" placeholder="">
             </div>
             <div class="d-flex flex-column align-items-center">
                 <button type="submit" class="btn btn-primary mt-4">Login</button>
