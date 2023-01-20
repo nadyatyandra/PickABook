84c274aa799d8d30f3f93b91a8093b9f80e3d59f
Tiff12 <tiffany.indryani@binus.ac.id>
2023-01-20 18:51:57 +0700

    new file:   public/images/commercial_1.jpeg 	new file:   public/images/commercial_2.jpeg 	modified:   resources/views/home.blade.php 	modified:   resources/views/layout/headerMember.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,17 +4,22 @@
 
 @section('main_body')
 
+<!-- commercials -->
 <div id="carouselAutoplaying" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselAutoplaying" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselAutoplaying" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    </div>
     <div class="carousel-inner">
         <div class="carousel-item active">
-        <img src="..." class="d-block w-100" alt="...">
+        <img src="{{asset('images/commercial_1.jpeg')}}" class="d-block w-100" alt="...">
         </div>
         <div class="carousel-item">
-        <img src="..." class="d-block w-100" alt="...">
+        <img src="{{asset('images/commercial_2.jpeg')}}" class="d-block w-100" alt="...">
         </div>
-        <div class="carousel-item">
+        <!-- <div class="carousel-item">
         <img src="..." class="d-block w-100" alt="...">
-        </div>
+        </div> -->
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselAutoplaying" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -25,5 +30,39 @@
         <span class="visually-hidden">Next</span>
     </button>
 </div>
+
+<!-- new release -->
+<div id="carouselRelease" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+    <div class="card container-fluid mt-3 w-75 pb-5" style="background-color: light">
+        <h1 class="text-center">New Release</h1>
+            <div class="card" style="width: 10rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="bookcard card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+                <a  id="booksbtn" href="#" class="booksbtn btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselRelease" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselRelease" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
+
+<style>
+    .booksbtn{
+        display:none;
+    }
+    .bookcard:hover + .booksbtn, .booksbtn:hover{
+        display: inline;
+    }
+</style>
 
 @endsection
--- resources/views/layout/headerMember.blade.php
+++ resources/views/layout/headerMember.blade.php
@@ -13,46 +13,45 @@
     integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Pick-A-Book</a>
+        <a class="navbar-brand fw-semibold  text-white" href="#">Pick-A-Book</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <a class="nav-link active text-white" aria-current="page" href="#">Home</a>
             </li>
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Categories
                 </a>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                <ul class="dropdown-menu bg-dark">
+                    <li><a class="dropdown-item text-white" href="#">Action</a></li>
+                    <li><a class="dropdown-item text-white" href="#">Another action</a></li>
+                    <li><a class="dropdown-item text-white" href="#">Something else here</a></li>
                 </ul>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Cart</a>
+                <a class="nav-link text-white" href="#">Cart</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">History</a>
+                <a class="nav-link text-white" href="#">History</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Profile</a>
+                <a class="nav-link text-white" href="#">Profile</a>
             </li>
         </ul>
         <form class="d-flex" role="search">
             <input class="form-control me-2" type="search" placeholder="Search book name/author/ISBN" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
+            <button class="btn btn-success" type="submit">Search</button>
         </form>
         </div>
     </div>
     </nav>
-    <div>
+    <div style="background-color: Coral">
         <br>
             @yield('main_body')
         <br>
