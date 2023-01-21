45bb88de9b8af3aae03fe6545374a8fe84ad352f
edwinabdiwijaya <edwin.abdiwijaya@binus.ac.id>
2023-01-21 15:29:33 +0700

    Fix Login page and continue register page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,15 +9,15 @@
             <h1 class="text-center">Login</h1>
             <div class="col-md-6 mb-4">
                 <label for="email" class="form-label">Email</label>
-                <input type="email" class="form-control" id="email">
+                <input type="email" class="form-control" id="email" placeholder="example@mail.com">
             </div>
             <div class="col-md-6 mb-4">
                 <label for="password" class="form-label">Password</label>
-                <input type="password" class="form-control" id="password" placeholder="">
+                <input type="password" class="form-control" id="password">
             </div>
             <div class="d-flex flex-column align-items-center">
                 <button type="submit" class="btn btn-primary mt-4">Login</button>
-                <p class="mt-4">Not have account? <a href="/register">Register</a></p>
+                <p class="mt-4">Not have account? <a href="/register">Register Now!</a></p>
             </div>
         </div>
     </form>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -3,5 +3,43 @@
 @section('title', 'Register')
 
 @section('body')
-    <h5>asd</h5>
+    <form action="" method="post">
+        @csrf
+        <div class="d-flex flex-column align-items-center container-fluid justify-content-center" style="background-color: bisque">
+            <h1 class="text-center">Register</h1>
+            <div class="col-md-6 mb-4">
+                <label for="nik" class="form-label">NIK</label>
+                <input type="number" class="form-control" id="nik" placeholder="(16 digits)">
+            </div>
+            <div class="col-md-6 mb-4">
+                <label for="name" class="form-label">Name</label>
+                <input type="text" class="form-control" id="name" placeholder="Your Name">
+            </div>
+            <div class="col-md-6 mb-4">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" placeholder="example@mail.com">
+            </div>
+            <div class="col-md-6 mb-4">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" placeholder="(min 5 letters)">
+            </div>
+            <div class="col-md-6 mb-4">
+                <label for="confirmed" class="form-label">Confirm Password</label>
+                <input type="password" class="form-control" id="confirmed" placeholder="(min 5 letters)">
+            </div>
+            <div class="col-md-6 mb-4">
+                <label for="address" class="form-label">Address</label>
+                <textarea class="form-control" id="address" placeholder="(min 5 letters)"></textarea>
+            </div>
+            {{-- DOB belum ada --}}
+            <div class="col-md-6 mb-4">
+                <label for="number" class="form-label">Phone Number</label>
+                <input type="number" class="form-control" id="number" placeholder="(9-14 digits)">
+            </div>
+            <div class="d-flex flex-column align-items-center">
+                <button type="submit" class="btn btn-primary mt-4">Register</button>
+                <p class="mt-4">Already have account? <a href="/login">Login Now!</a></p>
+            </div>
+        </div>
+    </form>
 @endsection
