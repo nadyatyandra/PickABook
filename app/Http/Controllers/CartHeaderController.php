8d5527ff21ada56c3ea8fbc6a8a9018056e28dc3
randyyz <ignasius_randy@yahoo.com>
2023-01-23 01:13:08 +0700

    extends BaseController

--- app/Http/Controllers/CartHeaderController.php
+++ app/Http/Controllers/CartHeaderController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class CartHeaderController extends Controller
+class CartHeaderController extends BaseController
 {
     public function cart(){
         return view('cart');
