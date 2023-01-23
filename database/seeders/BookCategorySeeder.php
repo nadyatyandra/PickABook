2096d6bdec6a6ef8db8cbc17da2711fa7773385e
FrederickGH <121942117+FrederickGH@users.noreply.github.com>
2023-01-23 13:35:21 +0700

    Update BookCategorySeeder.php

--- database/seeders/BookCategorySeeder.php
+++ database/seeders/BookCategorySeeder.php
@@ -23,6 +23,54 @@ class BookCategorySeeder extends Seeder
                 'bookId' => 2,
                 'categoryId' => 1
             ],
+            [
+                'bookId' => 3,
+                'categoryId' => 4
+            ],
+            [
+                'bookId' => 4,
+                'categoryId' => 4
+            ],
+            [
+                'bookId' => 4,
+                'categoryId' => 3
+            ],
+            [
+                'bookId' => 5,
+                'categoryId' => 4
+            ],
+            [
+                'bookId' => 6,
+                'categoryId' => 4
+            ],
+            [
+                'bookId' => 7,
+                'categoryId' => 4
+            ],
+            [
+                'bookId' => 8,
+                'categoryId' => 4
+            ],
+            [
+                'bookId' => 9,
+                'categoryId' => 5
+            ],
+            [
+                'bookId' => 10,
+                'categoryId' => 5
+            ],
+            [
+                'bookId' => 11,
+                'categoryId' => 4
+            ],
+            [
+                'bookId' => 12,
+                'categoryId' => 4
+            ],
+            [
+                'bookId' => 13,
+                'categoryId' => 4
+            ],
         ];
         foreach($book_categories as $key => $value){
             BookCategory::create($value);
