01f49a271f87e81ac8311900bb504b577203ae82
FrederickGH <121942117+FrederickGH@users.noreply.github.com>
2023-01-23 12:49:30 +0700

    Update AuthorSeeder.php

--- database/seeders/AuthorSeeder.php
+++ database/seeders/AuthorSeeder.php
@@ -14,66 +14,44 @@ class AuthorSeeder extends Seeder
      */
     public function run()
     {
-//         $authors = [
-//             [
-//                 'name' => 'Brandon Sanderson',
-//                 'photoPath' => 'brandon.jpg'
-//             ],
-//         ];
-        
-        Author::create([
-                'id' => 'AU001'
+        $authors = [
+            [
                 'name' => 'Masashi Kishimoto',
                 'photoPath' => 'kishimoto.jpg'
-            ]);
-        
-        Author::create([
-                'id' => 'AU002'
+            ],
+            [
                 'name' => 'Hirohiko Araki',
                 'photoPath' => 'araki.jpg'
-            ]);
-        
-        Author::create([
-                'id' => 'AU003'
+            ],
+            [
                 'name' => 'Shuichi Shigeno',
                 'photoPath' => 'shigeno.jpg'
-            ]);
-        
-        Author::create([
-                'id' => 'AU004'
+            ],
+            [
                 'name' => 'Hajime Isayama',
                 'photoPath' => 'isayama.jpg'
-            ]);
-        
-        Author::create([
-                'id' => 'AU005'
+            ],
+            [
                 'name' => 'Andrew Hussie',
                 'photoPath' => 'andrew.jpg'
-            ]);
-        
-        Author::create([
-                'id' => 'AU006'
+            ],
+            [
                 'name' => 'Sukismo',
                 'photoPath' => 'sukismo.jpg'
-            ]);
-        
-        Author::create([
-                'id' => 'AU007'
+            ],
+            [
                 'name' => 'Marthen Kaningan',
                 'photoPath' => 'marthen.jpg'
-            ]);
-        
-        Author::create([
-                'id' => 'AU008'
+            ],
+            [
                 'name' => 'James Dashner',
                 'photoPath' => 'james.jpg'
-            ]);
-        
-        Author::create([
-                'id' => 'AU009'
+            ],
+            [
                 'name' => 'Brandon Sanderson',
                 'photoPath' => 'brandon.jpg'
-            ]);
+            ],
+        ];
         
         foreach($authors as $key => $value){
             Author::create($value);
