774ed11bb0a161637ec3625cacd3dff9a2101565
Nadya Tyandra <nadyatyandra05@gmail.com>
2023-01-24 16:06:22 +0700

    dropdown contrast

--- resources/views/viewProfileAdmin.blade.php
+++ resources/views/viewProfileAdmin.blade.php
@@ -3,6 +3,6 @@
 @section('contentProfile')
 <h6 class="card-subtitle mb-2 mt-2">Library Name: {{$data->library->name}}</h6>
 <div class="d-grid gap-2 d-md-block">
-    <a class="btn btn-outline-primary" type="button" href="{{route('editPassword')}}">Edit Password</a>
+    <a class="btn btn-outline-dark" type="button" href="{{route('editPassword')}}">Edit Password</a>
 </div>
 @endsection
