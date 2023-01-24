7f910316de75b026d8c12e3c5ca6f3afdf58a494
Nadya Tyandra <nadyatyandra05@gmail.com>
2023-01-25 01:57:32 +0700

    minor ui rev

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -5,16 +5,16 @@
 @section('body')
 
 <div class="card container-fluid mt-3 w-75 pb-3 mb-3" style="background-color: light">
-    <h1  class="m-4">{{$name}}</h1>
-    <div class="m-4">
+    <h1 class="mt-3 px-3">{{$name}}</h1>
+    <div class="m-3">
         <button type="button" class="btn btn-outline-dark">Best Seller</button>
         <button type="button" class="btn btn-outline-dark">New Release</button>
-        <div class="d-flex flex-wrap m-4">
+        <div class="d-flex flex-wrap m-4 justify-content-center">
             @if ($books == '[]')
                 <h3>No Book Found...</h3>
             @endif
             @foreach ($books as $book)
-                <div class="card me-4 p-1" style="width: 10rem;">
+                <div class="card me-4 p-1 m-2" style="width: 10rem;">
                     <img src="{{url('storage\app\public\books\\'.$book->photoPath)}}" class="card-img-top" alt="...">
                     <div class="bookcard card-body">
                         <h5 class="card-title">{{$book->title}}</h5>
