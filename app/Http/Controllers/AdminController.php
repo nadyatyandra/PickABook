b8ce9b01cccf7ac397e6aceeae04dc72417eb538
randyyz <ignasius_randy@yahoo.com>
2023-01-24 16:19:46 +0700

    delete book from library

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Admin;
+use App\Models\BookLibrary;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
-    //
+    public function deleteBook($bookId){
+        $userId = Auth::user()->id;
+        $libraryId = Admin::where('userId', $userId)->first()->libraryId;
+        BookLibrary::where('bookId', '=', $bookId, 'and')->where('libraryId', $libraryId)->delete();
+
+        return redirect()->route('manageBook');
+    }
+
+    public function updateBook(){
+
+    }
+
+    public function insertBook(){
+
+    }
 }
