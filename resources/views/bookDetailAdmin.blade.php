6ec9e647be7e177205ccc273203086515c0d1a5d
Nadya Tyandra <nadyatyandra05@gmail.com>
2023-01-25 16:19:35 +0700

    author + button detail book admin

--- resources/views/bookDetailAdmin.blade.php
+++ resources/views/bookDetailAdmin.blade.php
@@ -1,6 +1,1 @@
 @extends('layout.fromBookDetailAdminMember')
-
-@section('details')
-    <a href="/updateBook" class="btn btn-outline-dark me-3">Update Book</a>
-    <a href="#" class="btn btn-outline-danger">Delete Book</a>
-@endsection
--- resources/views/booksByAuthor.blade.php
+++ resources/views/booksByAuthor.blade.php
@@ -3,26 +3,27 @@
 @section('title', 'Books By Author')
 
 @section('body')
-    <div class="card container-fluid mt-3 w-75 pb-3 mb-3" style="background-color: light">
-        <h1  class="m-4">About {{$name}}</h1>
-        <div class="row g-0">
-            <div class="col-md-4">
-                <img src="{{url('storage\app\public\authors\\'.$author->photoPath)}}" class="card-img-top m-4" alt="...">
-            </div>
-            <div class="col-md-8">
-            <div class="card-body">
-                <h5 class="card-title">Born</h5>
-                <p class="card-text">{{$author->birthPlace}}, {{$author->birthDate}}</p>
-                <h5 class="card-title">Biography</h5>
-                <p class="card-text">{{$author->biography}}</p>
-            </div>
+    <div class="d-flex flex-wrap justify-content-center">
+        <div class="card my-5 w-75 pt-4 pb-5 align-items-center">
+            <h1 class="mb-4">About {{$name}}</h1>
+            <div class="row align-items-center" style="width: 800px;">
+                <div class="col-md-4 px-1">
+                    <img src="{{url('storage\app\public\images\authors\\'.$author->photoPath)}}" class="img-fluid" alt="{{$author->photoPath}}">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Born</h5>
+                        <p class="card-text">{{$author->birthPlace}}, {{$author->birthDate}}</p>
+                        <h5 class="card-title">Biography</h5>
+                        <p class="card-text">{{$author->biography}}</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 
     <div class="card container-fluid mt-3 w-75 pb-3 mb-3" style="background-color: light">
-        <h1  class="m-4">Books By {{$name}}</h1>
-        <!-- <img src="{{url('storage\app\public\authors\\'.$author->photoPath)}}" class="card-img-top" alt="..."> -->
+        <h1  class="m-4 text-center">Books By {{$name}}</h1>
         <div class="m-4">
             <button type="button" class="btn btn-outline-dark">Best Seller</button>
             <button type="button" class="btn btn-outline-dark">New Release</button>
