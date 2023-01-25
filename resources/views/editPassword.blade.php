97734858a7f569448f9c4ecc2532a7432d31d331
Nadya Tyandra <nadyatyandra05@gmail.com>
2023-01-25 12:37:10 +0700

    login regis + cookie bug

--- resources/views/editPassword.blade.php
+++ resources/views/editPassword.blade.php
@@ -5,17 +5,17 @@
 @section('body')
 <form action="/profile/editPassword" method="post">
     @csrf
-    <div class="d-flex flex-column align-items-center container-fluid justify-content-center" style="background-color: bisque">
+    <div class='d-flex flex-column align-items-center rounded my-4 py-4 container-fluid' style="width: 600px; background-color: bisque">
         <h1 class="text-center my-3">Edit Password</h1>
-        <div class="col-md-6 mb-4">
+        <div class="col-md-6 mb-4 w-75">
             <label for="oldPassword" class="form-label">Old Password</label>
             <input type="password" class="form-control" name='oldPassword' id="oldPassword" placeholder="Old Password (min 5 letters)">
         </div>
-        <div class="col-md-6 mb-4">
+        <div class="col-md-6 mb-4 w-75">
             <label for="newPassword" class="form-label">New Password</label>
             <input type="password" class="form-control" name='newPassword' id="newPassword" placeholder="New Password (min 5 letters)">
         </div>
-        <div class="col-md-6 mb-4">
+        <div class="col-md-6 mb-4 w-75">
             <label for="confirmNewPassword" class="form-label">Confirm New Password</label>
             <input type="password" class="form-control" name='confirmNewPassword' id="confirmNewPassword" placeholder="Confirm New Password (min 5 letters)">
         </div>
--- resources/views/editProfile.blade.php
+++ resources/views/editProfile.blade.php
@@ -5,21 +5,21 @@
 @section('body')
 <form action="/profile/editProfile" method="post">
     @csrf
-    <div class="d-flex flex-column align-items-center container-fluid justify-content-center" style="background-color: bisque">
+    <div class='d-flex flex-column align-items-center rounded my-4 py-4 container-fluid' style="width: 600px; background-color: bisque">
         <h1 class="text-center my-3">Edit Profile</h1>
-        <div class="col-md-6 mb-4">
+        <div class="col-md-6 mb-4 w-75">
             <label for="name" class="form-label">Name</label>
             <input type="text" class="form-control" name='name' id="name" placeholder="Your Name" value="{{old('name')}}">
         </div>
-        <div class="col-md-6 mb-4">
+        <div class="col-md-6 mb-4 w-75">
             <label for="email" class="form-label">Email</label>
             <input type="email" class="form-control" name='email' id="email" placeholder="example@mail.com" value="{{old('email')}}">
         </div>
-        <div class="col-md-6 mb-4">
+        <div class="col-md-6 mb-4 w-75">
             <label for="address" class="form-label">Address</label>
             <textarea class="form-control" name='address' id="address" placeholder="(min 5 letters)"></textarea>
         </div>
-        <div class="col-md-6 mb-4">
+        <div class="col-md-6 mb-4 w-75">
             <label for="number" class="form-label">Phone Number</label>
             <input type="number" class="form-control" name='number' id="number" placeholder="(10-14 digits)" value="{{old('number')}}">
         </div>
