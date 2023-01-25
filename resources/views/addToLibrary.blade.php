d2c025f47a3be597a49ed5ac7c82e5bc490e17f7
LiamAcker <allschwrtz849@gmail.com>
2023-01-25 10:54:55 +0700

    follow up fix to add to library

--- resources/views/addToLibrary.blade.php
+++ resources/views/addToLibrary.blade.php
@@ -2,7 +2,7 @@
 
 @section('title', 'Add New Book to Library')
 
-@section('pageTitle', 'Add New Book toLibrary')
+@section('pageTitle', 'Add New Book to Library')
 
 @section('method')
     method="POST"
@@ -13,5 +13,5 @@
 @endsection
 
 @section('button')
-    <button type="submit" class="btn btn-dark mx-2">Insert</button>
+    <button type="submit" class="btn btn-dark mx-2">Add to Library</button>
 @endsection
